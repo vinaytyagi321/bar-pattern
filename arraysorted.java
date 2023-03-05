5e03b8928310305c1ecf6addb77ed7f93648e736
vinay <117964299+vinaytyagi321@users.noreply.github.com>
2023-03-05 09:04:47 +0530

    Add files via upload

---
+++ arraysorted.java
@@ -0,0 +1,17 @@
+public class arraysorted{
+public static boolean isarraysorted(int arr[]){
+    if(arr.length-1<=1){
+        return true;
+    }
+    for(int i=0; i<arr.length-1; i++){
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+public static void main(String[] args) {
+    int arr[] = {1,2,3,4,5,6,7,8};
+    System.out.println(isarraysorted(arr));
+}
+}
